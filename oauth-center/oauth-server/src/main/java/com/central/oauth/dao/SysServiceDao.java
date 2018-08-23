2a95efa991752c40402e78b9c6ae7a54e111155d
zhangzhiguang1 <986969368@qq.com>
2018-08-23 12:20:33 +0800

    init

--- oauth-center/oauth-server/src/main/java/com/central/oauth/dao/SysServiceDao.java
+++ oauth-center/oauth-server/src/main/java/com/central/oauth/dao/SysServiceDao.java
@@ -15,28 +15,28 @@ import java.util.List;
  * @Version: [1.0.0]
  * @Copy: [com.zzg]
  */
-//@Mapper
-//public interface SysServiceDao {
-//
-//    @Insert("insert into sys_service(parentId, name, url, path, css, sort, createTime, updateTime) "
-//            + "values (#{parentId}, #{name}, #{url} , #{path} , #{css}, #{sort}, #{createTime}, #{updateTime})")
-//    int save(SysMenu menu);
-//
-//    int update(SysMenu menu);
-//
-//    @Select("select * from sys_menu t where t.id = #{id}")
-//    SysMenu findById(Long id);
-//
-//    @Delete("delete from sys_menu where id = #{id}")
-//    int delete(Long id);
-//
-//    @Delete("delete from sys_menu where parentId = #{id}")
-//    int deleteByParentId(Long id);
-//
-//    @Select("select * from sys_menu t order by t.sort")
-//    List<SysMenu> findAll();
-//
-//    @Select("select * from sys_menu t where t.isMenu = 1 order by t.sort")
-//    List<SysMenu> findOnes();
-//
-//}
+@Mapper
+public interface SysServiceDao {
+
+    @Insert("insert into sys_service(parentId, name, url, path, css, sort, createTime, updateTime) "
+            + "values (#{parentId}, #{name}, #{url} , #{path} , #{css}, #{sort}, #{createTime}, #{updateTime})")
+    int save(SysMenu menu);
+
+    int update(SysMenu menu);
+
+    @Select("select * from sys_menu t where t.id = #{id}")
+    SysMenu findById(Long id);
+
+    @Delete("delete from sys_menu where id = #{id}")
+    int delete(Long id);
+
+    @Delete("delete from sys_menu where parentId = #{id}")
+    int deleteByParentId(Long id);
+
+    @Select("select * from sys_menu t order by t.sort")
+    List<SysMenu> findAll();
+
+    @Select("select * from sys_menu t where t.isMenu = 1 order by t.sort")
+    List<SysMenu> findOnes();
+
+}
