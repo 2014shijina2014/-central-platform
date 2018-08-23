2186f108bdf79501b4ce90eed88961fcc1d23373
zhangzhiguang1 <986969368@qq.com>
2018-08-23 12:21:49 +0800

    init

--- oauth-center/oauth-server/src/main/java/com/central/oauth/dao/SysServiceDao.java
+++ oauth-center/oauth-server/src/main/java/com/central/oauth/dao/SysServiceDao.java
@@ -1,6 +1,6 @@
 package com.central.oauth.dao;
 
-import com.central.model.user.SysMenu;
+import com.central.oauth.model.SysService;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -20,23 +20,23 @@ public interface SysServiceDao {
 
     @Insert("insert into sys_service(parentId, name, url, path, css, sort, createTime, updateTime) "
             + "values (#{parentId}, #{name}, #{url} , #{path} , #{css}, #{sort}, #{createTime}, #{updateTime})")
-    int save(SysMenu menu);
+    int save(SysService service);
 
-    int update(SysMenu menu);
+    int update(SysService service);
 
-    @Select("select * from sys_menu t where t.id = #{id}")
-    SysMenu findById(Long id);
+    @Select("select * from sys_service t where t.id = #{id}")
+    SysService findById(Long id);
 
-    @Delete("delete from sys_menu where id = #{id}")
+    @Delete("delete from sys_service where id = #{id}")
     int delete(Long id);
 
-    @Delete("delete from sys_menu where parentId = #{id}")
+    @Delete("delete from sys_service where parentId = #{id}")
     int deleteByParentId(Long id);
 
-    @Select("select * from sys_menu t order by t.sort")
-    List<SysMenu> findAll();
+    @Select("select * from sys_service t order by t.sort")
+    List<SysService> findAll();
 
-    @Select("select * from sys_menu t where t.isMenu = 1 order by t.sort")
-    List<SysMenu> findOnes();
+    @Select("select * from sys_service t where t.isMenu = 1 order by t.sort")
+    List<SysService> findOnes();
 
 }
