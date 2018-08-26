c00226fe10d4a921d45e1f69f228a132b49f52e3
GeekMyFell <986969368@qq.com>
2018-08-26 19:58:02 +0800

    init

--- monitor-center/zipkin-center/zipkin-center-native/src/main/java/com/central/zipkin/ZipkinCenterNativeApplication.java
+++ monitor-center/zipkin-center/zipkin-center-native/src/main/java/com/central/zipkin/ZipkinCenterNativeApplication.java
@@ -2,8 +2,10 @@ package com.central.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableZipkinServer		//默认采用HTTP通信方式启动ZipkinServer
 public class ZipkinCenterNativeApplication {
