ee47923e22826d960c244ce169115f4a203a45b7
GeekMyFell <986969368@qq.com>
2018-08-26 17:52:24 +0800

    init

--- monitor-center/zipkin-center/zipkin-center-mysql/src/main/java/com/cemtral/zipkin/ZipkinCenterMysqlApplication.java
+++ monitor-center/zipkin-center/zipkin-center-mysql/src/main/java/com/cemtral/zipkin/ZipkinCenterMysqlApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import zipkin.server.internal.EnableZipkinServer;
+import zipkin2.storage.mysql.v1.MySQLStorage;
 
 import javax.sql.DataSource;
+//import zipkin.server.internal.EnableZipkinServer;
+
 
 @SpringBootApplication
-@EnableZipkinServer		//默认采用HTTP通信方式启动ZipkinServer
+@EnableZipkinServer        //默认采用HTTP通信方式启动ZipkinServer
 public class ZipkinCenterMysqlApplication {
 
 	public static void main(String[] args) {
