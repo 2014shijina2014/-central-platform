aff95671ed554b9fc74234cb64ab0c24e3c4102f
GeekMyFell <986969368@qq.com>
2018-08-31 00:31:54 +0800

    init

---
+++ register-center/eureka-server/src/main/java/com/central/eureka/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.central.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: [zhangzhiguang]
+ * @Date: [2018-08-30 23:48]
+ * @Description: [ ]
+ * @Version: [1.0.0]
+ * @Copy: [com.zzg]
+ */
+@Configuration
+public class CorsConfig  {
+
+}
