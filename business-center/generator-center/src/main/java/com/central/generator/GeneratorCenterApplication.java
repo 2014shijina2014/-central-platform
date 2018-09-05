795a440b361e9cf7ef5934affdb800c93da57428
GeekMyFell <986969368@qq.com>
2018-09-05 10:50:01 +0800

    init

--- business-center/generator-center/src/main/java/com/central/generator/GeneratorCenterApplication.java
+++ business-center/generator-center/src/main/java/com/central/generator/GeneratorCenterApplication.java
@@ -1,8 +1,14 @@
 package com.central.generator;
 
+import com.central.annotation.EnableLogging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableLogging
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GeneratorCenterApplication {
 
