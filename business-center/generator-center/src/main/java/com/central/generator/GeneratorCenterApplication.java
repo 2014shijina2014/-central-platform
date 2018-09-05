d94df04b59b2e0df5a697e1476d5b11ce8101369
GeekMyFell <986969368@qq.com>
2018-09-05 10:46:47 +0800

    init

---
+++ business-center/generator-center/src/main/java/com/central/generator/GeneratorCenterApplication.java
@@ -0,0 +1,12 @@
+package com.central.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorCenterApplication.class, args);
+	}
+}
